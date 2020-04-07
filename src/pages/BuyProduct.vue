5da898044183b01f46dfda28cfe4db267c3719e2
samoralespu <samoralespu@unal.edu.co>
2020-04-07 18:36:17 -0500

    changes in buy product

--- src/pages/BuyProduct.vue
+++ src/pages/BuyProduct.vue
@@ -6,19 +6,24 @@
       >
         <md-card>
           <md-card-header data-background-color="green">
-            <h1 class="title">{{ product.name }}</h1>
-            <p class="category">{{ product.user}}</p>
+            <h1 class="title">{{ product.types[0].name }}</h1>
+            <p class="category">{{ product.types[0].user }}</p>
           </md-card-header>
           <div id="product">
             <div class="product">
-              <img v-bind:src="product.types[0].img"
-              class="img"/>
+              <img v-bind:src="product.types[0].img" class="img" alt="" />
               <div class="details">
-                <span v-if="product.quantity >= 20" class="stock in-stock">
+                <span
+                  v-if="product.types[0].quantity >= 20"
+                  class="stock in-stock"
+                >
                   Unidades disponibles
                 </span>
                 <span
-                  v-else-if="product.quantity > 0 && product.quantity < 20"
+                  v-else-if="
+                    product.types[0].quantity > 0 &&
+                      product.types[0].quantity < 20
+                  "
                   class="stock low-stock"
                 >
                   quedan pocas unidades
@@ -26,17 +31,8 @@
                 <span v-else class="stock out-of-stock">
                   agotado
                 </span>
-                <span>Select type:</span>
-                <div class="types">
-                  <span
-                    class="color-box"
-                    v-for="types in product.types"
-                    v-bind:key="types.id"
-                    v-bind:title="types.name"
-                    v-bind:style="{ backgroundColor: types.code }"
-                    v-bind:class="{ 'color-box': true}"
-                  >
-                  </span>
+                <div class="md-layout-item md-size-100 md-size-33">
+                  <h3 class="title">Precio: {{ product.types[0].price }}</h3>
                 </div>
               </div>
             </div>
@@ -46,23 +42,54 @@
           <md-card-header data-background-color="green">
             <h1 class="title">¡Compra!</h1>
           </md-card-header>
-          <div class="dr">
-            <p class="category">Unitario:</p>
-            <select v-model="selected">
-              <option disabled value="">Seleccione un la unidad</option>
-              <option>unidad uno</option>
-              <option>unidad dos</option>
-              <option>unidad tres</option>
-            </select>
-            <span>Seleccionado: {{ selected }}</span>
-            <p class="category">Precio total:</p>
-          </div>
+          <md-card-content>
+            <div class="md-layout">
+              <div class="md-layout-item md-small-size-100 md-size-33">
+                <md-field>
+                  <label>Numero de unidades</label>
+                  <md-input v-model="numberOfUnits" type="Number"></md-input>
+                </md-field>
+              </div>
+              <div class="md-layout-item md-small-size-100 md-size-33">
+                <md-field>
+                  <label for="country">Unidad</label>
+                  <md-select
+                    v-model="country"
+                    name="country"
+                    id="country"
+                    md-dense
+                  >
+                    <md-option value="Gramos">Gramos</md-option>
+                    <md-option value="Libras">Libras</md-option>
+                    <md-option value="Kilogramos">Kilogramos</md-option>
+                    <md-option value="Arrobas">Arrobas</md-option>
+                    <md-option value="Bultos(50 Kg)">Bultos(50 Kg)</md-option>
+                  </md-select>
+                </md-field>
+              </div>
+              <div class="md-layout-item md-small-size-100 md-size-33">
+                <h4></h4>
+              </div>
+              <div class="md-layout-item md-size-100 md-size-33">
+                <h3 class="title">
+                  Precio final: {{ product.types[0].price }}
+                </h3>
+              </div>
+              <div class="md-layout-item md-size-100 text-right">
+                <md-button class="md-raised md-success">Comprar</md-button>
+              </div>
+            </div>
+          </md-card-content>
         </md-card>
         <md-card>
           <md-card-header data-background-color="green">
             <h1 class="title">Descripción</h1>
           </md-card-header>
-          <p class="category">{{ product.description}}</p>
+          <div class="md-layout-item md-size-100 md-size-33">
+            <p class="category">
+              {{ product.types[0].description }}
+            </p>
+          </div>
         </md-card>
       </div>
     </div>
@@ -74,24 +101,28 @@ export default {
   name: "product",
   data: function() {
     return {
-      selected:'',
+      selected: "",
       product: {
-        name: "frutas",
-        description: "esta es la descripcion de una fruta",
-        quantity: 50,
         types: [
           {
             id: 1,
             name: "Fresa",
             user: "usuario1",
+            price: 50000,
             code: "#38393e",
+            quantity: 50,
+            description: "esta es la descripcion de una fruta",
             img:
               "https://ep01.epimg.net/elpais/imagenes/2020/02/26/estilo/1582729350_677456_1582730523_noticia_normal.jpg"
           },
           {
             id: 2,
             name: "Mango ",
+            user: "usuario1",
             code: "#696969",
+            price: 60000,
+            quantity: 50,
+            description: "esta es la descripcion de una fruta2",
             img:
               "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Mango_and_cross_section_edit.jpg/800px-Mango_and_cross_section_edit.jpg"
           }
