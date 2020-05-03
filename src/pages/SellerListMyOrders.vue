4103f3a29373a0f3fe81f220acba05ab3a90601d
samoralespu <samoralespu@unal.edu.co>
2020-05-02 22:16:59 -0500

    Name changed for some views and added new views

--- src/pages/SellerListMyOrders.vue
+++ src/pages/SellerListMyOrders.vue
@@ -1,59 +1,33 @@
 <template>
   <div class="content">
      <div class="md-layout">
-        <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-60">
+       <div class="md-layout-item text-center">
+          <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-60">
 
-          <md-card>
-            <md-card-header data-background-color="green">
-              <h1 class="title">¡Digita el usuario!</h1>
-            </md-card-header>
-            <md-card-content>
-              <div class="md-layout-item md-small-size-100 md-size-33">
-                <md-field>
-                  <label></label>
-                  <md-input
-                          id="ViewUserRequest"
-                          name="ViewUserRequest"
-                  >
-                  </md-input>
-                </md-field>
-              </div>
-              <div class="md-layout-item md-size-100 text-right">
-                <md-button
-                        v-on:click="saveCustomer"
-                        class="md-raised md-success"
-                        :disabled="numberOfUnits"
-                >
-                  Comprar
+            <md-list class="md-double-line  md-elevation-24" v-for="(request, index) in requests" v-bind:key="index">
+              <md-subheader>{{request.productName}}</md-subheader>
+              <md-divider></md-divider>
+              <md-list-item>
+                <md-icon class="md-primary md-size-2x">storefront</md-icon>
+
+                <div class="md-list-item-text">
+                  <span> Precio: {{request.totalPrice}}</span>
+                  <span>Unidades solicitadas:{{request.unit}}</span>
+                </div>
+              </md-list-item>
+
+              <md-list-item class="md-inset md-expand">
+                <div class="md-list-item-text">
+                  <span>{{request.description}}</span>
+                </div>
+
+                <md-button class="md-primary md-icon-button md-list-action">
+                    <span>Ver</span>
                 </md-button>
-              </div>
-            </md-card-content>
-          </md-card>
-
-          <md-list class="md-double-line  md-elevation-24" v-for="(request, index) in requests" v-bind:key="index">
-            <md-subheader>{{request.productName}}</md-subheader>
-            <md-divider></md-divider>
-            <md-list-item>
-              <md-icon class="md-primary md-size-2x">storefront</md-icon>
-
-              <div class="md-list-item-text">
-                <span> Precio: {{request.totalPrice}}</span>
-                <span>Unidades solicitadas:{{request.unit}}</span>
-              </div>
-            </md-list-item>
-
-            <md-list-item class="md-inset md-expand">
-              <div class="md-list-item-text">
-                <span>{{request.description}}</span>
-              </div>
-
-              <md-button class="md-primary md-icon-button md-list-action">
-                  <span>Ver</span>
-              </md-button>
-            </md-list-item>
-          </md-list>
-
-      </div>
+              </md-list-item>
+            </md-list>
+          </div>
+        </div>
     </div>
   </div>
 </template>
@@ -78,7 +52,7 @@ import http from "../http-common";
       getRequest(){
         console.log('metodo get request');
         axios
-                .get("http://localhost:8080/api/v1/request/user/ayuda@unal.edu.co")
+                .get("v1/order/seller/samoralespu@unal.edu.co")
                 .then(response => {
           console.log(response);
           this.requests=response.data
