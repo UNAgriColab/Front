4b04f4aebc4a2dad95039cc4992cdb892d124bcd
gapuentesr <gapuentesr@unal.edu.co>
2020-05-20 16:37:48 -0500

    :art: Changes in tables

--- src/components/Tables/BuyerListMyOrdersTable.vue
+++ src/components/Tables/BuyerListMyOrdersTable.vue
@@ -2,35 +2,87 @@
   <div>
     <md-table v-model="buyerOrders" :table-header-color="tableHeaderColor">
       <md-table-row>
-        <md-table-head>Oferta</md-table-head>
-        <md-table-head>Unidad</md-table-head>
-        <md-table-head>Número de unidades</md-table-head>
+        <md-table-head></md-table-head>
+        <md-table-head>Producto</md-table-head>
+        <md-table-head>Cantidad</md-table-head>
+        <md-table-head>Unidades</md-table-head>
         <md-table-head>Precio total</md-table-head>
+        <md-table-head>N° Ref</md-table-head>
+        <md-table-head>Estado</md-table-head>
+        <md-table-head>Acciones</md-table-head>
       </md-table-row>
       <md-table-row
         slot="md-table-row"
         v-for="(buyerOrder, index) in buyerOrders"
         v-bind:key="index"
       >
+        <md-table-cell md-label="Imágen">
+          <md-icon style="color: #58b05c">shopping_cart</md-icon>
+        </md-table-cell>
+        <md-table-cell md-label="Producto">
+          {{ buyerOrder.productName }}
+        </md-table-cell>
+        <md-table-cell md-label="Cantidad">
+          {{ buyerOrder.numberOfUnits }}
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-if="buyerOrder.unit === 1">
+          Gramos
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-else-if="buyerOrder.unit === 2">
+          Libras
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-else-if="buyerOrder.unit === 3">
+          Kilogramos
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-else-if="buyerOrder.unit === 4">
+          Arrobas
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-else-if="buyerOrder.unit === 5">
+          Bultos
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-else>
+          No asignado
+        </md-table-cell>
+        <md-table-cell md-label="Precio Total">
+          $ {{ buyerOrder.totalPrice.toFixed(2) }}
+        </md-table-cell>
         <md-table-cell md-label="Producto">
           {{ buyerOrder.offerReference }}
         </md-table-cell>
-        <md-table-cell md-label="Unidad">
-          {{ buyerOrder.unit }}
+        <md-table-cell md-label="Unidades" v-if="buyerOrder.state === 0">
+          Cancelado
         </md-table-cell>
-        <md-table-cell md-label="Cantidad minima">
-          {{ buyerOrder.numberOfUnits }}
+        <md-table-cell md-label="Unidades" v-else-if="buyerOrder.state === 1">
+          En espera
         </md-table-cell>
-        <md-table-cell md-label="Precio por unidad">
-          $ {{ buyerOrder.totalPrice }}
+        <md-table-cell md-label="Unidades" v-else-if="buyerOrder.state === 2">
+          En proceso
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-else-if="buyerOrder.state === 3">
+          Enviado
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-else-if="buyerOrder.state === 4">
+          Recibido
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-else>
+          No asignado
         </md-table-cell>
         <router-link to="/BuyerEditMyOrder" class="text-white">
           <md-button
-            class="md-raised md-success"
+            class="md-fab md-icon-button md-raised md-success"
             :value="buyerOrder.id"
             @click="addIdBuyerOrder(buyerOrder.id)"
           >
             <md-icon>create</md-icon>
+          </md-button>
+        </router-link>
+        <router-link to="" class="text-white">
+          <md-button
+            class="md-fab md-icon-button md-raised md-danger"
+            :value="buyerOrder.id"
+            @click="addIdBuyerOrder(buyerOrder.id)"
+          >
+            <md-icon>delete</md-icon>
           </md-button>
         </router-link>
       </md-table-row>
--- src/components/Tables/SellerListMyOffersTable.vue
+++ src/components/Tables/SellerListMyOffersTable.vue
@@ -2,37 +2,66 @@
   <div>
     <md-table v-model="sellerOffers" :table-header-color="tableHeaderColor">
       <md-table-row>
+        <md-table-head></md-table-head>
         <md-table-head>Producto</md-table-head>
-        <md-table-head>Presentación</md-table-head>
+        <md-table-head>Unidades</md-table-head>
         <md-table-head>Cantidad mínima</md-table-head>
         <md-table-head>Precio por presentación</md-table-head>
-        <!--<md-table-head>Acciones</md-table-head>-->
+        <md-table-head>Acciones</md-table-head>
       </md-table-row>
       <md-table-row
         slot="md-table-row"
         v-for="(sellerOffer, index) in sellerOffers"
         v-bind:key="index"
       >
+        <md-table-cell md-label="Imágen">
+          <md-icon style="color: #58b05c">eco</md-icon>
+        </md-table-cell>
         <md-table-cell md-label="Producto">
           {{ sellerOffer.productName }}
         </md-table-cell>
-        <md-table-cell md-label="Presentación">
-          {{ sellerOffer.presentation }}
+        <md-table-cell md-label="Unidades" v-if="sellerOffer.presentation === 1">
+          Gramos
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-else-if="sellerOffer.presentation === 2">
+          Libras
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-else-if="sellerOffer.presentation === 3">
+          Kilogramos
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-else-if="sellerOffer.presentation === 4">
+          Arrobas
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-else-if="sellerOffer.presentation === 5">
+          Bultos
+        </md-table-cell>
+        <md-table-cell md-label="Unidades" v-else>
+          No asignado
         </md-table-cell>
         <md-table-cell md-label="Cantidad mínima">
           {{ sellerOffer.minQuantity }}
         </md-table-cell>
-        <md-table-cell md-label="Precio por unidad">
+        <md-table-cell md-label="Precio por presentación">
           $ {{ sellerOffer.pricePresentation }}
         </md-table-cell>
         <md-table-cell md-label="Acciones">
           <router-link to="SellerEditMyOffer" class="text-white">
             <md-button
-              class="md-raised md-success"
+              class="md-fab md-icon-button md-raised md-success"
               :value="sellerOffer.id"
               @click="addIdOffer(sellerOffer.id)"
             >
               <md-icon>create</md-icon>
+            </md-button>
+          </router-link>
+
+          <router-link to="" class="text-white">
+            <md-button
+              class="md-fab md-icon-button md-raised md-danger"
+              :value="sellerOffer.id"
+              @click="addIdOffer(sellerOffer.id)"
+            >
+              <md-icon>delete</md-icon>
             </md-button>
           </router-link>
         </md-table-cell>
