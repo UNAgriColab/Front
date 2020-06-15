7a4cbb7d1f9a266eb3d7b869825c85a194f0243d
gapuentesr <gapuentesr@unal.edu.co>
2020-06-15 14:55:43 -0500

    :sparkles: Base Dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,62 +1,145 @@
 <template>
-  <div>
-    <md-dialog :md-active.sync="showDialog">
-      <md-dialog-title>Califica tu compra</md-dialog-title>
+  <div class="content">
+    <div class="md-layout">
+      <div
+        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
+      >
+        <stats-card data-background-color="green">
+          <template slot="header">
+            <md-icon>store</md-icon>
+          </template>
 
-      <div class="separator">
-        <md-avatar
-                class="md-avatar-icon md-large md-default"
-                style="margin-left: 10px"
-        >
-          <md-icon> star</md-icon>
-        </md-avatar>
-        &nbsp;
-        <md-avatar class="md-avatar-icon md-large md-default">
-          <md-icon> star</md-icon>
-        </md-avatar>
-        &nbsp;
-        <md-avatar class="md-avatar-icon md-large md-default">
-          <md-icon> star</md-icon>
-        </md-avatar>
-        &nbsp;
-        <md-avatar class="md-avatar-icon md-large md-default">
-          <md-icon> star</md-icon>
-        </md-avatar>
-        &nbsp;
-        <md-avatar
-                class="md-avatar-icon md-large md-default"
-                style="margin-right: 10px"
-        >
-          <md-icon> star</md-icon>
-        </md-avatar>
+          <template slot="content">
+            <p class="category">Ventas</p>
+            <h3 class="title">25</h3>
+          </template>
+
+          <template slot="footer">
+            <div class="stats">
+              <a href="">
+                <md-icon class="text-primary">playlist_add</md-icon>
+                Ir a mis pedidos</a
+              >
+            </div>
+          </template>
+        </stats-card>
       </div>
+      <div
+        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
+      >
+        <stats-card data-background-color="orange">
+          <template slot="header">
+            <md-icon>shopping_cart</md-icon>
+          </template>
 
-      <md-dialog-actions>
-        <md-button class="md-primary" @click="showDialog = false"
-        >Close</md-button
-        >
-        <md-button class="md-primary" @click="showDialog = false"
-        >Save</md-button
-        >
-      </md-dialog-actions>
-    </md-dialog>
+          <template slot="content">
+            <p class="category">Compras</p>
+            <h3 class="title">
+              49
+            </h3>
+          </template>
 
-    <md-button class="md-primary md-raised" @click="showDialog = true"
-    >CALIFICACIÓN</md-button
-    >
+          <template slot="footer">
+            <div class="stats">
+              <a href="">
+                <md-icon class="text-primary">add_to_photos</md-icon>
+                Ir a mis solicitudes</a
+              >
+            </div>
+          </template>
+        </stats-card>
+      </div>
+      <div
+        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
+      >
+        <stats-card data-background-color="red">
+          <template slot="header">
+            <md-icon>spa</md-icon>
+          </template>
+
+          <template slot="content">
+            <p class="category">Productos</p>
+            <h3 class="title">75</h3>
+          </template>
+
+          <template slot="footer">
+            <div class="stats">
+              <a href="">
+                <md-icon class="text-primary">shopping_cart</md-icon>
+                Ir a la lista de productos</a
+              >
+            </div>
+          </template>
+        </stats-card>
+      </div>
+      <div
+        class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
+      >
+        <stats-card data-background-color="blue">
+          <template slot="header">
+            <md-icon>account_circle</md-icon>
+          </template>
+
+          <template slot="content">
+            <p class="category">Mi perfil</p>
+            <h3 class="title">1/5</h3>
+          </template>
+
+          <template slot="footer">
+            <div class="stats">
+              <a href="">
+                <md-icon class="text-primary">person</md-icon>
+                Actualizar</a
+              >
+            </div>
+          </template>
+        </stats-card>
+      </div>
+      <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
+      >
+        <md-card>
+          <md-card-header data-background-color="green">
+            <h4 class="title">Ventas</h4>
+            <p class="category">Resumen de las últimas ventas realizadas</p>
+          </md-card-header>
+          <md-card-content>
+            <ordered-table table-header-color="green"></ordered-table>
+          </md-card-content>
+        </md-card>
+      </div>
+      <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
+      >
+        <md-card>
+          <md-card-header data-background-color="orange">
+            <h4 class="title">Compras</h4>
+            <p class="category">Resumen de las últimas compras realizadas</p>
+          </md-card-header>
+          <md-card-content>
+            <ordered-table table-header-color="orange"></ordered-table>
+          </md-card-content>
+        </md-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "DialogCustom",
-    data: () => ({
-      showDialog: false
-    })
-  };
-</script>
+import { StatsCard, OrderedTable } from "@/components";
 
-<style>
-  a:hover {
+export default {
+  components: {
+    StatsCard,
+    OrderedTable
+  },
+  data() {
+    return {
+      emailsSubscriptionChart: {
+        data: {},
+        options: {}
+      }
+    };
   }
-</style>
+};
+</script>
